e9bc0c40b41508d3112da6db2e9ac505e4c0c797
rajaahsan365 <113692614+rajaahsan365@users.noreply.github.com>
2024-05-28 15:00:58 +0500

    "Added Lusitana font from Google Fonts and exported it with weight and subsets configurations."

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,12 @@
-import { Inter, Poppins, Merriweather } from 'next/font/google';
+import { Inter, Poppins, Merriweather, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
 export const poppins = Poppins({ weight: ['400', '700'], subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
 
 export const merriweather = Merriweather({
   weight: ['400', '700'],
