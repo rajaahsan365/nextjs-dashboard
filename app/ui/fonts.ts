2e12cc90ad063fdd6bb2f41ac0f87bb6e768e6c8
rajaahsan365 <113692614+rajaahsan365@users.noreply.github.com>
2024-05-28 15:09:31 +0500

    "Added Merriweather import, updated lusitana export"

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,6 +3,7 @@ import { Inter, Poppins, Merriweather, Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
 
 export const poppins = Poppins({ weight: ['400', '700'], subsets: ['latin'] });
+
 export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
